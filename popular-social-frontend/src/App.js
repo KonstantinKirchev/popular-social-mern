df50827855551ca06a06ff69bc0709299622680b
Konstantin Kirchev <konstantin1230@yahoo.com>
2023-01-20 18:07:23 +0200

    Imported feed, sidebar and widget components into App component

--- popular-social-frontend/src/App.js
+++ popular-social-frontend/src/App.js
@@ -1,11 +1,30 @@
 import './App.css';
+import styled from 'styled-components'
+import Header from './components/Header'
+import Sidebar from './components/Sidebar'
+import Feed from './components/Feed'
+import Widget from './components/Widget'
 
 function App() {
   return (
     <div className="app">
-      <h1>Popular Social App</h1>
+      <AppWrapper>
+        <Header />
+        <div className="app__body">
+          <Sidebar />
+          <Feed />
+          <Widget />
+        </div>
+      </AppWrapper >
     </div>
   );
 }
+
+const AppWrapper = styled.div`
+  background-color: #f1f2f5;
+  .app__body {
+    display: flex;
+  }
+`
 
 export default App;
