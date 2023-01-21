d608c07fa168f46e01963aa5fdff914296a92255
Konstantin Kirchev <konstantin1230@yahoo.com>
2023-01-21 12:19:37 +0200

    Added pusher and 3 new API end points

--- popular-social-backend/server.js
+++ popular-social-backend/server.js
@@ -2,12 +2,13 @@ import express from 'express'
 import mongoose from 'mongoose'
 import cors from 'cors'
 import multer from 'multer'
-import GridFsStorage from 'multer-gridfs-storage'
+import {GridFsStorage} from 'multer-gridfs-storage'
 import Grid from 'gridfs-stream'
 import bodyParser from 'body-parser'
 import path from 'path'
 import Pusher from 'pusher'
 import dotenv from 'dotenv'
+import Posts from './postModel.js'
 
 //app config
 dotenv.config()
@@ -15,6 +16,14 @@ Grid.mongo = mongoose.mongo
 const app = express()
 const port = process.env.PORT || 9000
 const connection_url = process.env.CONNECTION_URL
+
+const pusher = new Pusher({
+    appId: "1541634",
+    key: "a5c32292d25551f7bcad",
+    secret: "dc0417cbcfe57c7267c8",
+    cluster: "eu",
+    useTLS: true
+});
 
 //middleware
 app.use(bodyParser.json())
@@ -26,11 +35,26 @@ const connection = mongoose.createConnection(connection_url, {
     useUnifiedTopology: true
 })
 
-const upload = multer({ storage })
-
+mongoose.set('strictQuery', false)
 mongoose.connect(connection_url, {
     useNewUrlParser: true,
     useUnifiedTopology: true
+})
+
+mongoose.connection.once('open', () => {
+    console.log('DB Connected for pusher')
+    const changeStream = mongoose.connection.collection('posts').watch()
+    changeStream.on('change', change => {
+        console.log(change)
+        if(change.operationType === "insert") {
+            console.log('Trigerring Pusher')
+            pusher.trigger('posts','inserted', {
+                change: change
+            })
+        } else {
+            console.log('Error trigerring Pusher')
+        }
+    })
 })
 
 let gfs
@@ -55,11 +79,49 @@ const storage = new GridFsStorage({
     }
 })
 
+const upload = multer({ storage })
+
 //api routes
 app.get("/", (req, res) => res.status(200).send("Hello TheWebDev"))
 
 app.post('/upload/image', upload.single('file'),(req, res) => {
     res.status(201).send(req.file)
+})
+
+app.get('/images/single',(req, res) => {
+    gfs.files.findOne({ filename: req.query.name }, (err, file) => {
+        if(err) {
+            res.status(500).send(err)
+        } else {
+            if(!file || file.length === 0) {
+                res.status(404).json({ err: 'file not found' })
+            } else {
+                const readstream = gfs.createReadStream(file.filename)
+                readstream.pipe(res)
+            }
+        }
+    })
+})
+
+app.post('/upload/post', (req, res) => {
+    const dbPost = req.body
+    Posts.create(dbPost, (err, data) => {
+        if(err)
+            res.status(500).send(err)
+        else
+            res.status(201).send(data)
+    })
+})
+
+app.get('/posts', (req, res) => {
+    Posts.find((err, data) => {
+        if(err) {
+            res.status(500).send(err)
+        } else {
+            data.sort((b,a) => a.timestamp - b.timestamp)
+            res.status(200).send(data)
+        }
+    })
 })
 
 //listen
