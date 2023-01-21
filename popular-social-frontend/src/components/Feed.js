4472165a1e87da6367605e5296a58516fee296ac
Konstantin Kirchev <konstantin1230@yahoo.com>
2023-01-21 12:22:52 +0200

    Remove the hardcoded posts and use pusher

--- popular-social-frontend/src/components/Feed.js
+++ popular-social-frontend/src/components/Feed.js
@@ -1,26 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Stories from './Stories'
 import styled from 'styled-components'
 import Messenger from './Messenger'
 import Post from './Post'
+import axios from '../axios'
+import Pusher from 'pusher-js'
 
 const Feed = () => {
+    const [postsData, setPostsData] = useState([])
+    const pusher = new Pusher('a5c32292d25551f7bcad', {
+        cluster: 'eu'
+    })
+    
+    const syncFeed = () => {
+        axios.get('/posts')
+            .then(res => {
+                console.log(res.data)
+                setPostsData(res.data)
+            })
+    }
+    
+    useEffect(() => {
+        const channel = pusher.subscribe('posts')
+        channel.bind('inserted', (data) => {
+            syncFeed()
+        }, [])
+    })
+
     return (
         <FeedWrapper>
             <Stories />
             <Messenger />
-            <Post profilePic="https://pbs.twimg.com/profile_images/1020939891457241088/fcbu814K_400x400.jpg"
-                message="Awesome post on CSS Animation. Loved it"
-                timestamp="1609512232424"
-                imgName="https://res.cloudinary.com/dxkxvfo2o/image/upload/v1598295332/CSS_Animation_xrvhai.png"
-                username="Nabendu"
-            />
-            <Post profilePic="https://pbs.twimg.com/profile_images/1020939891457241088/fcbu814K_400x400.jpg"
-                message="BookList app in Vanilla JavaScript"
-                timestamp="1509512232424"
-                imgName="https://res.cloudinary.com/dxkxvfo2o/image/upload/v1609138312/Booklist-es6_sawxbc.png"
-                username="TWD"
-            />
+            {
+                postsData.map(entry => (
+                    <Post
+                        profilePic={entry.avatar}
+                        message={entry.text}
+                        timestamp={entry.timestamp}
+                        imgName={entry.imgName}
+                        username={entry.user}/>
+                ))
+            }
         </FeedWrapper>
     )
 }
